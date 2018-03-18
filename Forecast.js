007d8225fdb692a79c5ea7f2443af252ee80b7c6
KEN-chan <hellokenta1024@gmail.com>
2018-03-18 12:52:47 -0400

    initial commit

---
+++ Forecast.js
@@ -0,0 +1,25 @@
+// @flow
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+class Forecast extends Component<{}> {
+
+  constructor() {
+
+  }
+
+  render() {
+
+    return (
+      <View style = { styles.container }>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});

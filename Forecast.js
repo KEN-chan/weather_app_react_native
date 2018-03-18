1b27a6ba658178947482822d857dc5d1d75b0272
KEN-chan <hellokenta1024@gmail.com>
2018-03-18 13:50:42 -0400

    add open weather map

--- Forecast.js
+++ Forecast.js
@@ -1,18 +1,24 @@
-// @flow
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
 class Forecast extends Component<{}> {
 
   constructor() {
-
+    super();
   }
 
   render() {
-
     return (
       <View style = { styles.container }>
-
+        <Text style = { styles.bigText }>
+          {this.props.main}
+        </Text>
+        <Text style = { styles.mainText }>
+          Current conditions: { this.props.description }
+        </Text>
+        <Text style = { styles.bigText }>
+          {this.props.temp} F
+        </Text>
       </View>
     );
   }
@@ -20,6 +26,19 @@ class Forecast extends Component<{}> {
 
 const styles = StyleSheet.create({
   container: {
+    height: 130,
+  },
+  bigText: {
+    flex: 2,
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    color: "#FFFFFF"
+  },
+  mainText: {
     flex: 1,
-  }
+    fontSize: 16,
+    textAlign: "center",
+    color: "#FFFFFF"
+  },
 });
--- WeatherProject.js
+++ WeatherProject.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Forecast from "./Forecast";
+import OpenWeatherMap from "./open_weather_map";
 
 type Props = {
 
@@ -18,17 +20,32 @@ class WeatherProject extends Component<Props, State> {
   }
 
   handleTextChange = event => {
+    console.log('value', event.nativeEvent.text);
+    console.log("yeah");
+    let zip = event.nativeEvent.text;
     this.setState({
       zip: event.nativeEvent.text
     });
   };
 
   render() {
+    let content = null;
+
+    if (this.state.forecast !== null) {
+      content = (
+        <Forecast main = { this.state.forecast.main }
+          description = { this.state.forecast.description }
+          temp = { this.state.forecast.temp }
+        />
+      );
+    }
+
     return (
       <View style = { styles.container }>
         <Text style = { styles.welcome }>
           You input { this.state.zip }
         </Text>
+        { content }
         <TextInput
           style = { styles.input }
           onSubmitEditing = { this.handleTextChange }
